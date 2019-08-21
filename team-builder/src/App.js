89239efe7b2603a44e24f20e4f97481afc11b8e4
Heorhii Siburov <g.siburov@gmail.com>
2019-08-21 13:53:55 -0700

    Team members was rendered, added semantic-ui

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import data from "./data";
 import Cards from "./components/Card";
 
+import { Header, Card, Container } from 'semantic-ui-react'
+
 function App() {
+  const [cards, setCards] = useState(data);
   return (
     <div className="App">
-      <header className="App-header"><h1>Team Builder</h1></header>
-      <div className="Main-cont">Forms will be here</div>
+      <Header as='h1' className="App-header">Team Builder</Header>
+      <Container text>
+      <Header as='h1' className="Main-cont">Forms will be here</Header>
+      </Container>
+      <Container text>
+        <Card.Group>
+          <Cards cardsList={cards} />
+        </Card.Group>
+      </Container> 
     </div>
   );
 }
--- team-builder/src/components/Card.js
+++ team-builder/src/components/Card.js
@@ -1,16 +1,20 @@
 import React from "react";
 
+import { Card } from 'semantic-ui-react'
+
 const Cards = props => {
   console.log(props);
   return (
     <div className="cards-list">
       {props.cardsList.map(card => {
         return (
-          <div className="cards" key={card.id}>
-            <h2>{card.name}</h2>
-            <p>{card.email}</p>
-            <p>{card.role}</p>
-          </div>
+            <Card className="cards" key={card.id}>
+                <Card.Content>
+                    <Card.Header>{card.name}</Card.Header>
+                    <Card.Meta>{card.email}</Card.Meta>
+                    <Card.Description>{card.role}</Card.Description>
+                </Card.Content>
+            </Card>
         );
       })}
     </div>
