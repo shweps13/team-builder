a4296ad94e08f8b5204a89705946184c28c5999d
Heorhii Siburov <g.siburov@gmail.com>
2019-08-21 12:56:24 -0700

    team data added

---
+++ team-builder/src/data.js
@@ -0,0 +1,33 @@
+export default [
+    {
+        id: 1,
+        name: "Stuart Spencer",
+        email: "stuart@gmail.com",
+        role: "Software engineer"
+    },
+    {
+        id: 2,
+        name: "Fred Barry",
+        email: "fred@gmail.com",
+        role: "Software engineer"
+    },
+    {
+        id: 3,
+        name: "Beulah Wardle",
+        email: "beulah@gmail.com",
+        role: "Software engineer"
+    },
+    {
+        id: 4,
+        name: "Indiana Conner",
+        email: "indiana@gmail.com",
+        role: "Front-end engineer"
+    },
+    {
+        id: 5,
+        name: "Evie-Grace Cobb",
+        email: "evie@gmail.com",
+        role: "Front-end engineer"
+    }
+  ];
+  
